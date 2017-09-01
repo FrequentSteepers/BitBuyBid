7da8af81c465601d62ccceeb971e179446f351f9
OneCent01 <jmpenney22@gmail.com>
2017-09-01 11:39:10 -0700

    setting up initial app component

--- client/src/app.js
+++ client/src/app.js
@@ -5,9 +5,34 @@ import store from './store';
 
 import App from '../src/components/App.jsx';
 
-render(
+
+import ReactDOM from 'react-dom';
+import { BrowserHistory, Route, Switch, Link } from 'react-router';
+import Home from './views/Home.jsx';
+
+class App extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Hello, there!</h2>
+        <Switch>
+          <Route exact path='/home' to={Home}/>
+          <Route path='/profile' to={Profile}/>
+          <Route path='/checkout' to={Checkout}/>
+        </Switch>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <BrowserHistory>
+      <App />
+    </BrowserHistory>
   </ Provider>, 
-  document.getElementById('root')
-);
+  document.getElementById('root'));

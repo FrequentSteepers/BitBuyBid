2db623b4808120e0a3af62b86183080e1b0c9b9a
OneCent01 <jmpenney22@gmail.com>
2017-09-01 19:47:46 -0700

    styling listings

--- client/src/components/Listing.jsx
+++ client/src/components/Listing.jsx
@@ -5,9 +5,11 @@ const Listing = (props) => {
   return (
     <div>
       <Card>
-        <span>{props.title}</span>
-        <span><img src={props.img}/></span>
-        <span>rating: {props.rating}</span>
+        <div>
+          {props.title}:
+          rating: {props.rating}
+          <img src={props.img}/>
+        </div>
       </Card>
     </div>
   );
--- client/src/components/Listings.jsx
+++ client/src/components/Listings.jsx
@@ -3,30 +3,60 @@ import {connect} from 'react-redux';
 import axios from 'axios';
 import {setProducts} from '../store/modules/products.js';
 import {bindActionCreators} from 'redux';
-
+import {GridList, GridTile} from 'material-ui/GridList';
 import Listing from './Listing.jsx';
+import $ from 'jquery';
+
+const styles = {
+  grid: {
+    width: 300,
+    height: 200,
+    overflowY: 'auto'
+  },
+  root: {
+    paddingTop: '10px',
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+  },
+  tile: {
+    maxHeight: '20%',
+    maxWidth: '20%',
+    padding: '0px'
+  }
+};
 
 const Listings = (props) => {
   const fetchProducts = () => {
     axios.get('/api/products')
       .then((results) => {
-        console.log(results.data);
+        let button = $('button');
+        console.log('data: ', results.data.results);
+        button[1].hidden = true;
         props.setProducts(results.data.results);
       });
   };
   return ( 
     <div>
       <button onClick={fetchProducts}>Click</button>
-      {(props.products).map((product) => {
-        return (
-          <Listing 
-            key={product.id} 
-            title={product.title}
-            img={product.imgs.small}
-            rating={product.rating}
-          />
-        );
-      })}
+      <div style={styles.root}>
+        {(props.products).map((product,i) => {
+          return (
+            <div key={i}>
+              <GridList style={styles.grid}>
+                <GridTile>
+                  <Listing 
+                    style={styles.tile}
+                    title={product.title}
+                    img={product.imgs.small}
+                    rating={product.rating}
+                  />
+                </GridTile>
+              </GridList>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
   

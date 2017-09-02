a267efc750e9c15b93092a4aa24ccc52ad5ce40b
OneCent01 <jmpenney22@gmail.com>
2017-09-01 18:02:49 -0700

    rebasing

--- client/src/components/Listing.jsx
+++ client/src/components/Listing.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 
 const Listing = (props) => {
   return (
     <div>
-      <span>{props.title}</span>
-      <span><img src={props.img}/></span>
-      <span>rating: {props.rating}</span>
+      <Card>
+        <span>{props.title}</span>
+        <span><img src={props.img}/></span>
+        <span>rating: {props.rating}</span>
+      </Card>
     </div>
   );
 };

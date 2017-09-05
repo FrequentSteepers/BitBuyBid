e9ab1dda7da9822d7de292cbfd75a534e6cc37a6
Clarence Bowen <cjb240@nyu.edu>
2017-09-05 14:09:18 -0700

    Integrate Amazon Advertising Search API

--- server/controllers/amazon.js
+++ server/controllers/amazon.js
@@ -12,7 +12,7 @@ module.exports = (req, res) => {
   }
 
   const access_key_id = process.env.access_key_id || cred.access_key_id;
-  const secret_key = process.env.secret_key || cred.secret_key ;
+  const secret_key = process.env.secret_key || cred.secret_key;
   const endpoint = 'webservices.amazon.com';
   const uri = '/onca/xml';
   const associateTag = process.env.associate_tag || cred.associate_tag;
@@ -20,21 +20,18 @@ module.exports = (req, res) => {
   const pairs = [];
   const product_list = [];
   const params = {
-		'Service': 'AWSECommerceService',
-		'Operation': 'ItemSearch',
-		'AWSAccessKeyId': access_key_id,
-		'AssociateTag': associateTag,
-		'SearchIndex': 'All',
-		'Keywords': searchTerm,
-		'ResponseGroup': 'Images,ItemAttributes' //, OfferSummary
-	};
+    'Service': 'AWSECommerceService',
+    'Operation': 'ItemSearch',
+    'AWSAccessKeyId': access_key_id,
+    'AssociateTag': associateTag,
+    'SearchIndex': 'All',
+    'Keywords': searchTerm,
+    'ResponseGroup': 'Images,ItemAttributes' //, OfferSummary
+  };
 
   const defaultImage = 'http://p5cdn4static.sharpschool.com/UserFiles/Servers/Server_5990980/Image/Wolf%20pack%20generic.png';
 
-  let keys, canonical_query_string, string_to_sign, hash, signature, request_url,
-  productListings;
-
-
+  let keys, canonical_query_string, string_to_sign, hash, signature, request_url, productListings;
 
   if (!params.hasOwnProperty('Timestamp')) {
     params['Timestamp'] = new Date().toISOString();
@@ -57,25 +54,24 @@ module.exports = (req, res) => {
   request_url = `http://${endpoint}${uri}?${canonical_query_string}&Signature=` + encodeURIComponent(signature);
 
   request.get({url: request_url}, function(err, response, body) {
-    if(err) { console.log('Error:', err); return; }
+    if (err) { console.log('Error:', err); return; }
 
     parseString(body, function (err, result) {
-        productListings = result.ItemSearchResponse.Items[0].Item;
+      productListings = result.ItemSearchResponse.Items[0].Item;
 
-        productListings.forEach((product) => {
-          product_list.push({
-            'id': product.ASIN[0], // ASIN
-            'small_image': product.SmallImage ? product.SmallImage[0].URL[0] : defaultImage,
-            'medium_image': product.MediumImage ? product.MediumImage[0].URL[0] : defaultImage,
-            'large_image': product.LargeImage ? product.LargeImage[0].URL[0] : defaultImage,
-            'url': product.DetailPageURL[0].substring(0, product.DetailPageURL[0].indexOf('?')),            'description': product.ItemAttributes[0].Title[0],
-            'list_price': product.ItemAttributes[0].ListPrice ? product.ItemAttributes[0].ListPrice[0].FormattedPrice[0].slice(1) : 'N/A',
-            'long_description': product.ItemAttributes[0].Feature ? product.ItemAttributes[0].Feature.join('; ') : ''
-          });
+      productListings.forEach((product) => {
+        product_list.push({
+          'id': product.ASIN[0], // ASIN
+          'small_image': product.SmallImage ? product.SmallImage[0].URL[0] : defaultImage,
+          'medium_image': product.MediumImage ? product.MediumImage[0].URL[0] : defaultImage,
+          'large_image': product.LargeImage ? product.LargeImage[0].URL[0] : defaultImage,
+          'url': product.DetailPageURL[0].substring(0, product.DetailPageURL[0].indexOf('?')),
+          'description': product.ItemAttributes[0].Title[0],
+          'list_price': product.ItemAttributes[0].ListPrice ? product.ItemAttributes[0].ListPrice[0].FormattedPrice[0].slice(1) : 'N/A',
+          'long_description': product.ItemAttributes[0].Feature ? product.ItemAttributes[0].Feature.join('; ') : ''
         });
-
-        res.send(product_list)
+      });
+      res.send(product_list);
     });
-
   });
 };

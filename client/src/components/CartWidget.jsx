315b17ffa9e5083b17c3fb9735704adf2989fad2
OneCent01 <jmpenney22@gmail.com>
2017-09-06 19:46:13 -0700

    feat/organized elements in cart widget

--- client/src/components/CartWidget.jsx
+++ client/src/components/CartWidget.jsx
@@ -10,12 +10,26 @@ const style = {
     position: 'fixed',
     top: '10%',
     right: '5%',
-    'zIndex': 2,
-    width: 200
+    zIndex: 2,
   },
   paper: {
-    position: 'relative',
+    border: '2px solid black',
+    width: '250px'
+  },
+  header: {
     textAlign: 'center',
+    margin: '0 auto'
+  },
+  title: {
+    padding: '0px',
+    whiteSpace: 'nowrap',
+    overflow: 'hidden',
+  },
+  text: {
+    padding: '0px'
+  },
+  grid: {
+    border: '2px solid black'
   }
 };
 
@@ -25,6 +39,8 @@ const mapStateToProps = state => {
   };
 };
 
+// will be using this dispatch once the listings are connected to 
+// increment/decrement/remove functionality:
 // const mapDispatchToProps = state => {
 //   return bindActionCreators({}, dispatch);
 // }
@@ -36,31 +52,30 @@ class CartWidget extends Component {
   render() {
     return (
       <div style={style.root}>
-        <Paper style={style.paper}>
-          <h2>Cart</h2>
-          <Grid fluid>
-            <Row >
+        {this.props.cart.length ? <Paper style={style.paper}>
+          <h2 style={style.header}>Cart</h2>
+          <Grid style={style.grid} fluid>
+            <Col xs={12}>
               {this.props.cart ? this.props.cart.map((product, i) => {
-                <Col key={i} xs={6} sm={6} md={4} lg={3}>
-                  <div>Title: {this.props.title}</div>
-                  <Card>
-                    <CardHeader
-                      title="URL Avatar"
-                      subtitle="Subtitle"
-                      avatar={`${product.imgs.small}`}
-                    />
-                    <CardMedia
-                      overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle" />}
-                    >
-                      <img src={`${product.imgs.small}`} alt="" />
-                    </CardMedia>
-                    <CardTitle title="Card title" subtitle="Card subtitle" />
-                  </Card>
-                </Col>;
+                return (
+                  <div key={i}>
+                    <Row style={style.row} start="xs">
+                      <Col xs={4}>
+                        <CardMedia>
+                          <img src={`${product.imgs.small}`} alt="" />
+                        </CardMedia>
+                      </Col>
+                      <Col xs={8}>
+                        <CardTitle style={style.title} title={`${product.title}`}/>
+                        <CardText style={style.text}>This is where the description of the product will go! :D</CardText>
+                      </Col>
+                    </Row>
+                  </div>
+                );
               }) : false}
-            </Row>
+            </Col>
           </Grid>
-        </Paper>
+        </Paper> : false}
       </div>
     );
   }

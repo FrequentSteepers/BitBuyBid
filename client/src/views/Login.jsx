801abb7593c751d2a326006b4e6a180d1a541e86
Adisa Stewart <adisa.stewart1988@gmail.com>
2017-09-07 19:29:01 -0700

    Refactor Login view to stateful component that tracks input information on change

--- client/src/views/Login.jsx
+++ client/src/views/Login.jsx
@@ -4,37 +4,56 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import {Link} from 'react-router-dom';
 
-const Login = (props) => {
-  return (
-    <div className="col-sm-6 col-sm-offset-3">
-      <h1><span className="fa fa-sign-in"></span>Login</h1>
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      password: '',
+      email: ''
+    };
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handlePassChange = this.handlePassChange.bind(this);
+  }
+  handleEmailChange(e) {
+    this.setState({email: e.target.value});
+  }
 
-      <form onSubmit={handleLogin}>
+  handlePassChange(e) {
+    this.setState({password: e.target.value});
+  }
+
+  render() {
+    return (
+      <div className="col-sm-6 col-sm-offset-3">
+        <h1><span className="fa fa-sign-in"></span>Login</h1>     
 
         <div className="form-group">
           <label>Email</label>
-          <input type="text" className="form-control" name="email" />
+          <input onChange={this.handleEmailChange} type="text" className="form-control" name="email" />
         </div>
 
         <div className="form-group">
           <label>Password</label>
-          <input type="password" className="form-control" name="password" />
+          <input onChange={this.handlePassChange} type="password" className="form-control" name="password" />
         </div>
 
-        <button type="submit" className="btn btn-warning btn-lg">Login</button>
+        <button onClick={() => { 
+          this.props.handleLogin({email: this.state.email, password: this.state.password});
+        }} 
+        className="btn btn-warning btn-lg">Login</button>
 
-      </form>
+        <p>Need to sign up for an account?</p>
+        <Link to='/signup'>Sign up Page</ Link>
+      </div>
+    );
+  }
 
-      <p>Need to sign up for an account?</p>
-      <Link to='/signup'>Sign up Page</ Link>
-    </div>
-  );
-};
+}
 
 const mapStateToProps = (state) => {
-  // return {
-  //   term: state.search.term
-  // };
+  return {
+    // term: state.search.term
+  };
 };
 const mapDispatchToProps = (dispatch) => {
   return bindActionCreators({

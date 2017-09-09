349f2f6d0974533ff79c511ad5b065866747784f
OneCent01 <jmpenney22@gmail.com>
2017-09-08 18:51:07 -0700

    spacing

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,4 +3,4 @@ module.exports.auth = require('./auth');
 module.exports.users = require('./users');
 module.exports.products = require('./products');
 module.exports.transactions = require('./transactions');
-module.exports.search = require('./search');
+module.exports.search = require('./search'); 

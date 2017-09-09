99ebb333d5ea543bbb918c0b8f61dfa8a71c5bb5
Adisa Stewart <adisa.stewart1988@gmail.com>
2017-09-09 12:44:30 -0700

    Initiate bitExchange function

---
+++ server/controllers/bitExchange.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+// export default bitExchange = function () {
+//   axios.get('https://api.coinbase.com/v2/prices/spot?currency=USD')
+//     .then((data) => {
+//       console.log(data);
+//     })
+//     .catch(console.error);
+// };
+module.exports.bitExchange = 2;

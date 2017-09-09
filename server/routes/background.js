a87c472c9c3ac8360ec436d7dc5cbfee95b6e2bd
OneCent01 <jmpenney22@gmail.com>
2017-09-08 18:57:34 -0700

    delete unused redux file

--- server/routes/background.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const express = require('express');
-const router = express.Router();
-const {getOne} = require('../controllers').Background;
-
-router.route('/')
-  .get(getOne);
-
-module.exports = router;

51b0ca2dc217544bf28900751edab14bd879ae64
OneCent01 <jmpenney22@gmail.com>
2017-09-12 15:01:51 -0700

    Refactor/receipt component no longer passed state

--- client/src/views/Cart.jsx
+++ client/src/views/Cart.jsx
@@ -10,31 +10,12 @@ import {
 import Listing from '../components/Listing.jsx';
 import Receipt from '../views/Receipt.jsx';
 import CartItem from '../components/CartItem.jsx';
+import Subtotal from '../components/Subtotal.jsx';
 import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 
 class Checkout extends Component {
   constructor(props) {
     super(props);
-    this.calculateTotal = this.calculateTotal.bind(this);
-  }
-
-  /**
-   * Calculate the total amount to charge for the items in cart (pre tax)
-   */
-  calculateTotal() {
-    return this.props.cart.reduce((acc, curr) => 
-      acc + (Number(curr.price)) * this.props.quantities[curr.prod_id]
-      , 0).toFixed(2);
-  }
-  
-  /**
-   * Calculate the amount of bitcoins to buy after rounding up. 
-   */
-  calculateLeftOver() {
-    const total = this.props.cart.reduce((acc, curr) => 
-      acc + (Number(curr.price)) * this.props.quantities[curr.prod_id]
-      , 0).toFixed(2);
-    return (Math.ceil(total) - total).toFixed(2);
   }
 
   render() {
@@ -44,7 +25,7 @@ class Checkout extends Component {
           {this.props.cart.map((item, i) => {
             return <CartItem key={i} item={item} />;
           })}
-          <div>Subtotal: ${this.calculateTotal()}
+          <div>Subtotal: <Subtotal/>
           </div>
           <Switch>
             <Route exact path='/cart/confirm'>
@@ -57,7 +38,7 @@ class Checkout extends Component {
               <Link to='/cart/confirm'><button>Checkout</button></Link>
             </Route>
             <Route path='/receipt'>
-              <Receipt amount={this.calculateLeftOver()} />
+              <Receipt/>
             </Route>
           </Switch>
         </div> 

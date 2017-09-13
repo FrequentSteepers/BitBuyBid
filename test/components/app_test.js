51d4e7297b455b32dc8544f9fa40c1067b540bc4
m <mgarber93@gmail.com>
2017-09-12 19:14:07 -0700

    Enable travis test coverage

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { mount, shallow } from 'enzyme';
 import { expect } from 'chai';
-import App from '../../src/components/app';
+import App from '../../src/views/app.jsx';
+
+import configureStore from '../../src/store/index.js';
+
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+const store = configureStore(window.__PRELOADED_STATE__, window);
 
 const setup = () => {
-  const wrapper = mount(<App />);
+  const wrapper = mount(<Provider store={store}><App /></Provider>);
 
   return {
     wrapper,

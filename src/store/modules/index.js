c0bab13823189e67b872aa5b43bbea22678e1a2f
Adisa Stewart <adisa.stewart1988@gmail.com>
2017-09-14 15:35:37 -0700

    Correct spelling in filepath

--- src/store/modules/index.js
+++ src/store/modules/index.js
@@ -2,6 +2,6 @@ import {combineReducers} from 'redux';
 import app from './app';
 import products from './products';
 import search from './search';
-import exchange from './exhange';
+import exchange from './exchange';
 
 export default combineReducers({app, search, products, exchange});

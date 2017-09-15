c0df06383cdd9ec8d0503dc231912b5ddf83e460
m <mgarber93@gmail.com>
2017-09-15 15:21:40 -0700

    Abstract amazon signiture logic

--- server/controllers/amazon.js
+++ server/controllers/amazon.js
@@ -1,61 +1,55 @@
 const { Transaction } = require('../../db/models');
 const axios = require('axios');
 
-const { 
-  associate_tag,
-  access_key_id,
-  secret_key,
-  signRequest
-} = require('../../config/api_configs/amazon');
+const {buildAmazonRequest} = require('../../utils').amazon;
+
+const convert = require('xml-js');
 
 /**
  * Create an amazon cart from a transaction in the database
  */
-module.exports.create = (req, res) => {
-  // req.body.transaction_id
-  Transaction.where({id: 1}) 
+module.exports.getOne = (req, res) => {
+  Transaction.where({id: req.params.id}) 
     .fetch({
       withRelated: ['cart', 'buyer']
     })
     .then(transaction => {
-      var url = 'http://webservices.amazon.com/onca/xml?' +
-      'Service=AWSECommerceService&' +
-      `AWSAccessKeyId=${access_key_id}&` +
-      'Operation=CartCreate&'; 
       var c = 1; 
       const items = transaction.relations.cart.models
         .map(o => o._previousAttributes);
+      let params = {
+        Operation: 'CartCreate',
+        Service: 'AWSECommerceService'
+      };
       for (const item of items) {
         if (item.asin && item._pivot_quantity) {
-          url += `Item.${c}.ASIN=${item.asin}&`;
-          url += `Item.${c}.Quantity=${item._pivot_quantity}&`;
+          params[`Item.${c}.ASIN`] = item.asin;
+          params[`Item.${c}.Quantity`] = item._pivot_quantity;
           c++;
         }
       }
       if (c === 1) {
         throw new Error('no data!');
       }
-      console.log(signRequest(url));
-      res.status(200).end();
+      return axios.get(buildAmazonRequest(params));
+    })
+    .then(results => {
+      const parsed = JSON.parse(convert.xml2json(results.data,
+        {
+          compact: true,
+          spaces: 2,
+          instructionHasAttributes: true
+        }
+      ));
+      const response = {
+        amzn_cart_id: parsed.CartCreateResponse.Cart.CartId._text,
+        amzn_HMAC: parsed.CartCreateResponse.Cart.HMAC._text,
+        amzn_URLEncodedHMAC: parsed.CartCreateResponse.Cart.URLEncodedHMAC._text,
+      };
+      transaction.save(response).then(() => console.log('success!'));
     })
     .catch(err => {
       console.error(err);
       res.status(500).end();
     });
-};
-
-module.exports.getAll = (req, res) => {
-  res.status(500);
-};
-
-module.exports.getOne = (req, res) => {
-  res.status(500);
-};
-
-module.exports.update = (req, res) => {
-  res.status(500);
-};
-
-module.exports.deleteOne = (req, res) => {
-  res.status(500);
 };

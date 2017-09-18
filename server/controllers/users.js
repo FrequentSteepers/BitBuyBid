40fc5d72710c91c349aa04b0dc0d246f8b388f9d
m <mgarber93@gmail.com>
2017-09-17 18:55:34 -0700

    Implement create active cart controller

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -61,21 +61,61 @@ module.exports.update = (req, res) => {
     });
 };
 
-// module.exports.deleteOne = (req, res) => {
-//   models.Profile.where({ id: req.params.id }).fetch()
-//     .then(profile => {
-//       if (!profile) {
-//         throw profile;
-//       }
-//       return profile.destroy();
-//     })
-//     .then(() => {
-//       res.sendStatus(200);
-//     })
-//     .error(err => {
-//       res.status(503).send(err);
-//     })
-//     .catch(() => {
-//       res.sendStatus(404);
-//     });
-// };
+module.exports.createActiveCart = (req, res) => {
+  if (!req.body.cart || req.body.cart.length === 0) {
+    res.status(405).send('You must have a cart');
+  }
+  if (req.params.id !== req.user.id) {
+    res.status(401).send('You must be logged in');
+  }
+  let builtTransaction = {};
+  Transaction.forge(
+    { 
+      buyer_id: req.user.id		
+    }
+  )
+    .save()
+    .then(result => {
+      builtTransaction = result;
+      builtTransaction.cart = [];
+      return Promise.all(
+        req.body.cart.map(
+          p => {
+            Purchase.forge({
+              transaction_id: result.id, 
+              product_id: p.id,
+              quantity: req.body.quantities[p.prod_id] || 1
+            })
+              .save();
+          }
+        )
+      );
+    })
+    .then(result => {
+      return models.User.where({id: req.params.id})
+        .fetch()
+        .then(profile => {
+          if (!profile) {
+            throw profile;
+          }
+          return profile.save(
+            {
+              'active_cart': builtTransaction.id
+            }, 
+            { 
+              method: 'update' 
+            }
+          );
+        });
+    })
+    .then(() => {
+      res.json(builtTransaction).sendStatus(201);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).send(err);
+    })
+    .error(err => {
+      res.status(500).send(err);
+    });
+};

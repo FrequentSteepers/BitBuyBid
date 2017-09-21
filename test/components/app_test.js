ee6f5c1c6f0eb07b85aa6c836e6ac0d7b7696d4b
Adisa Stewart <adisa.stewart1988@gmail.com>
2017-09-21 16:00:23 -0700

    Add test for rendering of muithemeprovider and browserrouter

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { mount, shallow } from 'enzyme';
 import { expect } from 'chai';
 import App from '../../src/views/app.jsx';
+import AppRoot from '../../src/components/app-root.jsx';
 
 import configureStore from '../../src/store/index.js';
 
@@ -10,18 +11,32 @@ import { Provider } from 'react-redux';
 
 const store = configureStore(window.__PRELOADED_STATE__, window);
 
-const setup = () => {
-  const wrapper = mount(<Provider store={store}><App /></Provider>);
+// const setup = () => {
+//   const wrapper = mount(<Provider store={store}><App /></Provider>);
 
-  return {
-    wrapper,
-  };
-};
+//   return {
+//     wrapper,
+//   };
+// };
 
 describe('App', () => {
+  const setup = () => {
+    const wrapper = mount(<Provider store={store}><App /></Provider>);
+
+    return {
+      wrapper,
+    };
+  };
   const { wrapper } = setup();
-  console.log(wrapper);
+  // console.log(wrapper);
   it('renders something', () => {
     expect(wrapper).to.exist;
   });
+  it('MuiThemeProvider renders', () => {
+    expect(wrapper.find('MuiThemeProvider').length).to.equal(1);
+  });
+  it('BrowserRouter renders', () => {
+    expect(wrapper.find('BrowserRouter').length).to.equal(1);
+  });
 });
+

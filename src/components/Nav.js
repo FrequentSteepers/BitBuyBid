b6bf21d45997a7a9d5dae505d3829233e4236e1a
OneCent01 <jmpenney22@gmail.com>
2017-09-22 09:41:52 -0700

    feat/conditional rendering links in Nav bar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -27,6 +27,9 @@ const style = {
   },
   link: {
     textDecoration: 'none'
+  },
+  label: {
+    color: 'white'
   }
 };
 
@@ -73,29 +76,31 @@ class Navbar extends React.Component {
           iconElementRight={
             this.props.user ? 
               <div>
-                <Link to ='/profile'><FlatButton label={this.props.user.username} labelStyle={{color: 'white'}} /></Link> 
-                <Link to ='/logout' ><FlatButton label="Logout" labelStyle={{color: 'white'}} onClick={this.props.handleLogout} /></Link>
+                <Link to ='/profile'><FlatButton label={this.props.user.username} labelStyle={style.label} /></Link> 
+                <Link to ='/logout' ><FlatButton label="Logout" labelStyle={style.label} onClick={this.props.handleLogout} /></Link>
               </div> :
               <div>
-                <Link to ='/signup' ><FlatButton label="Signup" labelStyle={{color: 'white'}} /></Link>
-                <Link to ='/login' ><FlatButton label="Login" labelStyle={{color: 'white'}} /></Link>
+                <Link to ='/signup' ><FlatButton label="Signup" labelStyle={style.label} /></Link>
+                <Link to ='/login' ><FlatButton label="Login" labelStyle={style.label} /></Link>
               </div>
           }
         />
         <Drawer docked={false} width={250} open={this.state.open} onRequestChange={(open) => this.setState({open})}>
           <Link style={style.link} to='/'><MenuItem>Search</MenuItem></Link>
-          <Link style={style.link} to='/profile'><MenuItem>Profile</MenuItem></Link>
-          <Link style={style.link} to='/cart'><MenuItem >Cart</MenuItem></Link>
-          <Link style={style.link} to='/' onClick={this.props.handleLogout}><MenuItem>Logout</MenuItem></Link>
-          <Link style={style.link} to='/login'><MenuItem>Login</MenuItem></Link>
+          {this.props.user ? <Link style={style.link} to='/profile'><MenuItem>Profile</MenuItem></Link> : null}
+          {this.props.cart && this.props.cart.length ? <Link style={style.link} to='/cart'><MenuItem >Cart</MenuItem></Link> : null}
+          {this.props.user ? 
+            <Link style={style.link} to='/' onClick={this.props.handleLogout}><MenuItem>Logout</MenuItem></Link> : 
+            <Link style={style.link} to='/login'><MenuItem>Login</MenuItem></Link>}
         </Drawer>
       </div>
     );
   }
 }
-const mapStateToProps = ({app, transactions}) => ({
+const mapStateToProps = ({app, transactions, products}) => ({
   user: app.user,
-  pendingTransaction: transactions.pendingTransaction
+  pendingTransaction: transactions.pendingTransaction,
+  cart: products.cart
 });
 
 const mapDispatchToProps = (dispatch) => {
